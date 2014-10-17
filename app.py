996828737e2247b438a9c1d0e469b5994cd4e362
Alex Enriquez <djalex12@hotmail.com>
2014-10-17 16:51:19 -0500

    subidos script y vista

---
+++ app.py
@@ -0,0 +1,65 @@
+__author__ = 'alexis'
+import sys,datetime
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
+from PyQt4.QtNetwork import QTcpServer,QTcpSocket
+from PyQt4.phonon import Phonon
+
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.UIGenerator()
+        self.Bindings()
+        #self.videoPlayer.show()
+    def UIGenerator(self):
+        self.mainLayout=QGridLayout()
+        self.videoPlayer=Phonon.VideoPlayer()
+        self.btnPlay=QPushButton("Play")
+        self.btnStop=QPushButton("Stop")
+        self.btnPuase=QPushButton("Pause")
+        self.duration=QProgressBar()
+        self.mainLayout.addWidget(self.videoPlayer,0,0,2,3)
+        self.mainLayout.addWidget(self.duration,3,0,1,3)
+        self.mainLayout.addWidget(self.btnPlay,4,0)
+        self.mainLayout.addWidget(self.btnStop,4,1)
+        self.mainLayout.addWidget(self.btnPuase ,4,2)
+        self.setLayout(self.mainLayout)
+        self.setMinimumSize(500,500)
+        self.setMinimumSize(500,500)
+
+    def Bindings(self):
+        self.duration.setValue(0)
+        self.btnPlay.clicked.connect(self.Reproducir)
+        self.btnStop.clicked.connect(self.Detener)
+        self.btnPuase.clicked.connect(self.Pausar)
+
+    def Reproducir(self):
+
+        self.videoPlayer.load(Phonon.MediaSource("You Could Be Mine.mp4"))
+        self.videoPlayer.play()
+
+    def Detener(self):
+        self.videoPlayer.stop()
+
+    def Pausar(self):
+
+        if self.videoPlayer.isPaused():
+            self.videoPlayer.play()
+        else:
+            self.videoPlayer.pause()
+
+
+
+if __name__=="__main__":
+
+    try:
+        app=QApplication(sys.argv)
+        app.setStyle("Plastique")
+        ventana=Window()
+        ventana.show()
+        sys.exit(app.exec_())
+    except SystemExit:
+        print "You have exited"
+
+
