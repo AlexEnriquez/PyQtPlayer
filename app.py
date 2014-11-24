cec12f8d3bc894fb5a4eb81815fa613a7f9b7e89
Alex Enríquez <AlexEnriquez@users.noreply.github.com>
2014-11-23 22:55:01 -0500

    app actualizada

--- app.py
+++ app.py
@@ -1,60 +1,55 @@
-__author__ = 'Alex Enriquez'
-import sys
+__author__ = 'Alex'
 from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from PyQt4.phonon import Phonon
+from  PyQt4.phonon import Phonon
+import sys
+from PyQt4 import uic
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
-        self.UIGenerator()
-        self.Bindings()
-   
-   # se encarga de Instanciar los controles
-    def UIGenerator(self):
-        self.mainLayout=QGridLayout()
-        self.videoPlayer=Phonon.VideoPlayer()
-        self.btnPlay=QPushButton("Play")
-        self.btnStop=QPushButton("Stop")
-        self.btnPuase=QPushButton("Pause")
-        self.duration=QProgressBar()
-        self.mainLayout.addWidget(self.videoPlayer,0,0,2,3)
-        self.mainLayout.addWidget(self.duration,3,0,1,3)
-        self.mainLayout.addWidget(self.btnPlay,4,0)
-        self.mainLayout.addWidget(self.btnStop,4,1)
-        self.mainLayout.addWidget(self.btnPuase ,4,2)
-        self.setLayout(self.mainLayout)
-        self.setMinimumSize(500,500)
-        
-    #Ejecuta los bindings de cada control
-    def Bindings(self):
-        self.duration.setValue(0)
+
+        #SE CARGA LA VISTA O INTERFAZ GRAFICA
+        uic.loadUi("window.ui",self)
+
+        #LLAMA  a los binding para conectar con la ui
+        self.mediaSource=Phonon.MediaSource("aqui se pone el video")#los videos soportados por el momento son wvm
+        self.videoPlayer.load(self.mediaSource)
+        #auto reproduce el video
+        self.videoPlayer.play()
+
+        #BINDINGS DE BOTONES
         self.btnPlay.clicked.connect(self.Play)
         self.btnStop.clicked.connect(self.Stop)
-        self.btnPuase.clicked.connect(self.Pause)
+        self.btnPause.clicked.connect(self.Pause)
+        self.volumen.valueChanged.connect(self.Volumen)
 
-    #Metodo que reproduce el video
     def Play(self):
-        self.videoPlayer.load(Phonon.MediaSource("Here Put the name of your video.*"))#fomrato soportado por le momento wmv
-        self.videoPlayer.play()
-        
-    #Metodo que detiene el video
+        if self.videoPlayer.isPlaying():
+            pass
+        else:
+            self.videoPlayer.play()
+
     def Stop(self):
         self.videoPlayer.stop()
-        
-    #Metodo que pausa el video
+
     def Pause(self):
         if self.videoPlayer.isPaused():
             self.videoPlayer.play()
         else:
             self.videoPlayer.pause()
 
+    def Volumen(self):
+        volum=(float(self.volumen.value())*float(self.volumen.maximum()))/10000
+        self.videoPlayer.setVolume(float(volum))
+
+    def Salir(self):
+        app.exit()
+
 if __name__=="__main__":
     try:
         app=QApplication(sys.argv)
-        app.setStyle("Plastique")
         ventana=Window()
         ventana.show()
         sys.exit(app.exec_())
     except SystemExit:
-        print "Thanks for use it"
+        ventana.Salir()
